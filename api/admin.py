1bcf45c01112ecaf9b7e437fd5fe986413a1edc5
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2025-08-01 23:55:30 +0600

    feat: admin route added

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import CustomerViewSet
+
+router = DefaultRouter()
+router.register(r'customers', CustomerViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('admin/', admin.site.urls),
+]
