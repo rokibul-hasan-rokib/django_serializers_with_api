ce7516393c1f3ce82b79b8e62e194c96ae033172
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2025-08-01 23:51:25 +0600

    unique email vlidaton fix

--- api/serializers.py
+++ api/serializers.py
@@ -2,6 +2,12 @@ from rest_framework import serializers
 from .models import Customer
 
 class CustomerSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(required=True)
+
+    def validate_email(self, value):
+        if Customer.objects.filter(email=value).exists():
+            raise serializers.ValidationError("Email already exists.")
+        return value
     class Meta:
         model = Customer
         fields = ['id', 'name', 'email', 'phone', 'created_at', 'updated_at']
