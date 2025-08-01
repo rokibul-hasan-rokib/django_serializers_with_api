c366ecdf6eea7e1fe9fc91549e711194b5004596
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2025-08-01 23:57:38 +0600

    feat : orders serializers added with validations

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Customer
+from .models import Order
 
 class CustomerSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(required=True)
@@ -12,3 +13,14 @@ class CustomerSerializer(serializers.ModelSerializer):
         model = Customer
         fields = ['id', 'name', 'email', 'phone', 'created_at', 'updated_at']
         read_only_fields = ['created_at', 'updated_at']
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = ['id', 'customer', 'product_name', 'quantity', 'order_date']
+        read_only_fields = ['order_date']
+    
+    def validate_quantity(self, value):
+        if value <= 0:
+            raise serializers.ValidationError("Quantity must be a positive integer.")
+        return value
