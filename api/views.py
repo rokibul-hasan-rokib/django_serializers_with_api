62d6cbfc756aa0c85d7016820b780b85804622bd
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2025-08-01 23:53:23 +0600

    feat: django backend filter added

--- api/views.py
+++ api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from rest_framework.response import Response
 from .models import Custmer
 
@@ -7,3 +7,6 @@ from .serializers import CustmerSerializer
 class CustmerViewSet(viewsets.ModelViewSet):
     queryset = Custmer.objects.all()
     serializer_class = CustmerSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name', 'email']
+
