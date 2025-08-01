c79360e14bf456390bd15b6f22010a6f0ab0d8bd
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2025-08-01 23:48:20 +0600

    feat: view added

--- api/views.py
+++ api/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .models import Custmer
 
-# Create your views here.
+from .serializers import CustmerSerializer
+
+class CustmerViewSet(viewsets.ModelViewSet):
+    queryset = Custmer.objects.all()
+    serializer_class = CustmerSerializer
