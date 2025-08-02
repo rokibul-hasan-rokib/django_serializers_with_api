dd221761e616cff03df8dd494d564ce6064e82ba
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2025-08-02 16:45:28 +0600

    feat: serializers added in oders

--- api/serializers.py
+++ api/serializers.py
@@ -26,4 +26,4 @@ class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
         fields = ['id', 'name', 'email', 'phone', 'created_at', 'updated_at']
-        read_only_fields = ['created_at', 'updated_at']
+        read_only_fields = ['created_at', 'updated_at'] 
